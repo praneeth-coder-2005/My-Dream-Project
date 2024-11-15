3fe59e0b4a56cf91f5769aa80bc7cce35cab0317
praneeth-coder-2005 <savediskofc@gmail.com>
2024-11-15 19:09:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -210,5 +210,5 @@ def start_telegram_bot():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+app.run(host="0.0.0.0", port=port, debug=False)
     start_telegram_bot()
