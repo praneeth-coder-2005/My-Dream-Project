12381f9d6c8c05f2bd52e9d01d04ff88b43853f4
praneeth-coder-2005 <savediskofc@gmail.com>
2024-11-16 10:18:38 +0530

    Delete features directory

--- features/__init__.py
+++
@@ -1,1 +0,0 @@
-# This is an initializer for the features module
--- features/edit_delete.py
+++
@@ -1,7 +0,0 @@
-def handle_edit_post():
-    # Your edit post functionality
-    pass
-
-def handle_delete_post():
-    # Your delete post functionality
-    pass
